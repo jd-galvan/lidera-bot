02d4c7a10e61d208d453114aaa2789fa603d738f
jd-galvan <jdgsuazo@gmail.com>
2024-07-17 16:09:09 -0500

    Avoid https validation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/providers/theme-provider";
 import { Toaster } from "@/components/ui/sonner"
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -25,10 +26,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
+      </head>
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="dark">
           {children}
-        <Toaster />
+          <Toaster />
         </ThemeProvider>
       </body>
     </html>
