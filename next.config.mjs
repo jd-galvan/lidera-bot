92b024db04f767ffe25ca739e7f2865d541d9749
jd-galvan <jdgsuazo@gmail.com>
2024-07-18 08:10:56 -0500

    Revert "Http proxy"
    This reverts commit 00fb6899223538f3f6e5cc108ee4c272659e00a8.

--- next.config.mjs
+++ next.config.mjs
@@ -1,48 +1,27 @@
 /** @type {import('next').NextConfig} */
-const { createProxyMiddleware } = require('http-proxy-middleware');
-
 const nextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: '/api/tags',
-        destination: 'http://110.238.69.52:11434/api/tags',
-      },
-    ];
-  },
-  async serverMiddleware() {
-    const proxy = createProxyMiddleware('/api/tags', {
-      target: 'http://110.238.69.52:11434',
-      changeOrigin: true,
-      pathRewrite: {
-        '^/api/tags': '/api/tags',
-      },
-    });
-
-    return proxy;
-  },
   output: 'standalone',
   webpack: (config, { isServer }) => {
-    // Fixes npm packages that depend on `fs` module
-    if (!isServer) {
-      config.resolve.fallback = {
-        ...config.resolve.fallback, // if you miss it, all the other options in fallback, specified
-        // by next.js will be dropped. Doesn't make much sense, but how it is
-        fs: false, // the solution
-        module: false,
-        perf_hooks: false,
-      };
-    }
-
-    return config
-  },
-  typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
-    ignoreBuildErrors: true,
-  },
+        // Fixes npm packages that depend on `fs` module
+        if (!isServer) {
+          config.resolve.fallback = {
+            ...config.resolve.fallback, // if you miss it, all the other options in fallback, specified
+              // by next.js will be dropped. Doesn't make much sense, but how it is
+            fs: false, // the solution
+            module: false,
+            perf_hooks: false,
+          };
+        }
+    
+        return config
+      },
+      typescript: {
+        // !! WARN !!
+        // Dangerously allow production builds to successfully complete even if
+        // your project has type errors.
+        // !! WARN !!
+        ignoreBuildErrors: true,
+      },
 };
 
 
