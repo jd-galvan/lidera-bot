7ad735543d68ef067455dd213bb716c10b4486b8
jd-galvan <jdgsuazo@gmail.com>
2024-07-18 08:09:25 -0500

    Revert "Revert changes"
    This reverts commit 8b9f136ee3308505c6268460175476e0fb4da004.

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,26 @@
 /** @type {import('next').NextConfig} */
+import { createProxyMiddleware } from 'http-proxy-middleware'
+
 const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/api/tags',
+        destination: 'http://110.238.69.52:11434/api/chat',
+      },
+    ];
+  },
+  async serverMiddleware() {
+    const proxy = createProxyMiddleware('/api/chat', {
+      target: 'http://110.238.69.52:11434',
+      changeOrigin: true,
+      pathRewrite: {
+        '^/api/chat': '/api/chat',
+      },
+    });
+
+    return proxy;
+  },
   output: 'standalone',
   webpack: (config, { isServer }) => {
     // Fixes npm packages that depend on `fs` module
@@ -23,3 +44,7 @@ const nextConfig = {
     ignoreBuildErrors: true,
   },
 };
+
+
+
+export default nextConfig;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/providers/theme-provider";
 import { Toaster } from "@/components/ui/sonner"
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -25,6 +26,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
+      </head>
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="dark">
           {children}
